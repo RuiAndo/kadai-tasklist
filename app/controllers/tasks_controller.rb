caf783e61ba52cdeecfc2527a198d1b0b72804a0
RuiAndo <onlyisnotlonelyrui@yahoo.co.jp>
2017-05-07 09:13:16 +0000

    add column status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,12 +47,15 @@ class TasksController < ApplicationController
   end
   
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
-  def set_task
-    @tasks = Task.find(params[:id])
-  end
+
   
 end
